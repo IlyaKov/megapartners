8b3c480b79637c3bdd6c86bb7d0877b8c7f4ae69
IlyaKov <svarkaed@yandex.ru>
2023-03-25 17:14:08 +0100

    mobile adaptive

--- index.js
+++ index.js
@@ -91,3 +91,18 @@ const brands = new Splide( '#brands .splide', {
 } );
 
 brands.mount();
+
+const icon = document.querySelector('#nav-icon3')
+icon.addEventListener('click', ({ target }) => {
+  const sidebar = document.querySelector('.sidebar')
+  const callback = (element) => {
+    element.classList.toggle('open')
+    sidebar.classList.toggle('open')
+  }
+
+  if (target.tagName === 'DIV') {
+    callback(target)
+  } else {
+    callback(target.parentNode)
+  }
+}, true)
